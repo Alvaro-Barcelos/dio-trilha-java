2beb85ce7274e342b8b80f92b925ea9318faa17b
Alvaro <alvaropassalto@gmail.com>
2024-03-18 10:35:35 -0300

    update

--- Aulas/src/edu/alvaro/javadoc/comentario.java
+++ Aulas/src/edu/alvaro/javadoc/comentario.java
@@ -1,35 +1,27 @@
 package edu.alvaro.javadoc;
 
 public class comentario {
-	// Comentario de uma linha
-	
-	/*
-	 * javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java
-	 * Esta linha não esta dando certo
-	 * Comentario de varias linhas!
-	 */
-	
-	   /**
-	    * Estas duas estrelas são para metodos
-	    */
-	public void metodo() {
-		
-	}
-	/*
-	 * Este método foi elaborado as pressas
-	 * por isso eu abrevei o nome das variáveis
-	 * mas olha, ele tem a finalidade de somar ou  multiplicar
-	 * dois números
-	 */
-	
-	public int somaMultiplica (int n, int x, String m){
-	    int r = 0; // r é igual ao resultado
-	    if (m == "M"){ // M= multiplicação
-	        r= n * x;
-	    }else{
-	        // se não soma mesmo
-	        r = n + x;
+	/**
+	* <h1>Calculadora</h1>
+	* A Calculadora realiza operações matemáticas entre números inteiros
+	* <p>
+	* <b>Note:</b> Leia atentamente a documentação desta classes
+	* para desfrutar dos recursos oferecidos pelo autor
+	*
+	* @author  Gleyson Sampaio
+	* @version 1.0
+	* @since   01/01/2022
+	*/
+	public class Calculadora {
+	    /**
+	   * Este método é utilizado para somar dois números inteiros
+	   * @param numeroUm este é o primeiro parâmetro do método
+	   * @param numeroDois este é o segundo parâmetro do método
+	   * @return int o resultado deste método é a soma dos dois números.
+	   */
+	    public int somar(int numeroUm, int numeroDois) {
+	        return  numeroUm + numeroDois;
 	    }
-	    return r;
 	}
+
 }
